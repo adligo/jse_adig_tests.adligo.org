fa9cbc8952d30a7ee9b7e3249b668fa1f9cfc769
scott <scott@adligo.com>
2012-01-24 03:59:18 +0000

    adding in tests

--- src/org/adligo/i/jse_adig/CreateDirectoryTests.java
+++ src/org/adligo/i/jse_adig/CreateDirectoryTests.java
@@ -2,6 +2,7 @@ package org.adligo.i.jse_adig;
 
 import java.io.File;
 
+import org.adligo.i.adi.client.InvocationException;
 import org.adligo.i.adig.client.GRegistry;
 import org.adligo.i.adig.client.I_GCheckedInvoker;
 import org.adligo.i.log.client.Log;
@@ -31,5 +32,59 @@ public class CreateDirectoryTests extends ATest {
 		File [] children = file.listFiles();
 		assertEquals(1, children.length);
 		assertEquals("testCreateDirectory", children[0].getName());
+	}
+	
+	public void testCreateDirectoryFailureEmpty() {
+		
+		I_GCheckedInvoker<String, Boolean> invoker = GRegistry.getCheckedInvoker(JseInvokerNames.CREATE_DIRECTORY_INVOKER, 
+				String.class, Boolean.class);
+		JseRegistry.setup();
+		InvocationException caught = null;
+		try {
+			invoker.invoke(null);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(CreateDirectoryInvoker.class.getName() + 
+				CreateDirectoryInvoker.REQUIRES_A_NON_NULL_DIRECTORY_STRING,
+				caught.getMessage());
+		
+		caught = null;
+		try {
+			invoker.invoke(" ");
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(CreateDirectoryInvoker.class.getName() + 
+				CreateDirectoryInvoker.REQUIRES_A_NON_NULL_DIRECTORY_STRING,
+				caught.getMessage());
+		
+	}
+	
+	public void testCreateDirectoryTwice() throws Exception {
+		String newDir = DirectorySetup.getTestDirPath() + File.separator + "testCreateDirectory";
+		File file = new File(DirectorySetup.getTestDirPath());
+		I_GCheckedInvoker<String, Boolean> invoker = GRegistry.getCheckedInvoker(JseInvokerNames.CREATE_DIRECTORY_INVOKER, 
+				String.class, Boolean.class);
+		JseRegistry.setup();
+		
+		assertNotNull(invoker);
+		assertTrue(invoker.invoke(newDir));
+		File [] children = file.listFiles();
+		assertEquals(1, children.length);
+		assertEquals("testCreateDirectory", children[0].getName());
+		
+		InvocationException caught = null;
+		try {
+			invoker.invoke(newDir);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(CreateDirectoryInvoker.class.getName() + 
+				CreateDirectoryInvoker.WAS_NOT_ABLE_TO_CREATE_THE_DIRECTORY_S + newDir,
+				caught.getMessage());
 	}
 }
---
+++ src/org/adligo/i/jse_adig/MoveFileTests.java
@@ -0,0 +1,180 @@
+package org.adligo.i.jse_adig;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.adligo.i.adi.client.InvocationException;
+import org.adligo.i.adig.client.GRegistry;
+import org.adligo.i.adig.client.I_GCheckedInvoker;
+import org.adligo.tests.ATest;
+
+public class MoveFileTests extends ATest {
+	private I_GCheckedInvoker<MoveFileToken, Boolean> invoker = 
+		GRegistry.getCheckedInvoker(JseInvokerNames.MOVE_FILE_INVOKER, 
+		MoveFileToken.class, Boolean.class);
+	
+	public void setUp() {
+		DirectorySetup.setUp();
+		JseRegistry.setup();
+	}
+	
+	public void tearDown() {
+		DirectorySetup.tearDown();
+	}
+	
+	public void testEmptyToFile() {
+		
+		MoveFileToken token = new MoveFileToken();
+		InvocationException caught = null;
+		try {
+			invoker.invoke(token);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(MoveFileInvoker.class.getName() + 
+				MoveFileInvoker.REQUIRES_A_NON_EMPTY_TO_FILE_PATH_IN_THE_MOVE_FILE_TOKEN,
+				caught.getMessage());
+		
+	}
+	
+	public void testEmptyFromFile() {
+		
+		MoveFileToken token = new MoveFileToken();
+		token.setTo_file_path("hey.txt");
+		InvocationException caught = null;
+		try {
+			invoker.invoke(token);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(MoveFileInvoker.class.getName() + 
+				MoveFileInvoker.REQUIRES_A_NON_EMPTY_FROM_FILE_PATH_IN_THE_MOVE_FILE_TOKEN,
+				caught.getMessage());
+		
+	}
+	
+	public void testFromDoesNOTExist() {
+		
+		MoveFileToken token = new MoveFileToken();
+		token.setTo_file_path("hey.txt");
+		token.setFrom_file_path("bar.txt");
+		InvocationException caught = null;
+		try {
+			invoker.invoke(token);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(MoveFileInvoker.class.getName() + 
+				MoveFileInvoker.REQUIRES_A_VALID_FILE_PATH_IN_THE_MOVE_FILE_TOKEN_BUT_THE_FILE_DOESN_T_EXIST,
+				caught.getMessage());
+	}
+	
+	public void testToParentDirectoryDoesNOTExist() throws IOException {
+		String newDir = DirectorySetup.getTestDirPath() + File.separator + "testToParentDirectoryDoesNOTExist";
+		File file = new File(newDir);
+		assertTrue(file.mkdirs());
+		
+		String childA = newDir + File.separator + "a";
+		File fileA  = new File(childA);
+		
+		String childB = newDir + File.separator + "b.txt";
+		File fileB  = new File(childB);
+		assertTrue(fileB.createNewFile());
+		
+		MoveFileToken token = new MoveFileToken();
+		token.setTo_file_path(childA + File.separator + "a.txt");
+		token.setFrom_file_path(childB);
+		InvocationException caught = null;
+		try {
+			invoker.invoke(token);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(MoveFileInvoker.class.getName() + 
+				MoveFileInvoker.REQUIRES_A_TO_FILE_PATH_WITH_A_EXISTING_PARENT_DIRECTORY,
+				caught.getMessage());
+	}
+	
+	public void testFailMoveFileBetweenDirs() throws IOException {
+		String newDir = DirectorySetup.getTestDirPath() + File.separator + "testToParentDirectoryDoesNOTExist";
+		File file = new File(newDir);
+		assertTrue(file.mkdirs());
+		
+		String childA = newDir + File.separator + "a";
+		File fileA  = new File(childA);
+		assertTrue(fileA.mkdirs());
+		
+		String childAB = newDir + File.separator + "a" + File.separator + "b.txt";
+		File fileAB  = new File(childAB);
+		assertTrue(fileAB.createNewFile());
+		
+		String childB = newDir + File.separator + "b.txt";
+		File fileB  = new File(childB);
+		assertTrue(fileB.createNewFile());
+		
+		MoveFileToken token = new MoveFileToken();
+		token.setTo_file_path(childAB);
+		token.setFrom_file_path(childB);
+		InvocationException caught = null;
+		try {
+			invoker.invoke(token);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(MoveFileInvoker.class.getName() + 
+				MoveFileInvoker.CAN_NOT_CURRENTLLY_MOVE_A_FILE_BETWEEN_DIRECTORIES ,
+				caught.getMessage());
+	}
+	
+	public void testFailMoveFileOntoSelf() throws IOException {
+		String newDir = DirectorySetup.getTestDirPath() + File.separator + "testToParentDirectoryDoesNOTExist";
+		File file = new File(newDir);
+		assertTrue(file.mkdirs());
+		
+		
+		String childB = newDir + File.separator + "b.txt";
+		File fileB  = new File(childB);
+		assertTrue(fileB.createNewFile());
+		
+		MoveFileToken token = new MoveFileToken();
+		token.setTo_file_path(childB);
+		token.setFrom_file_path(childB);
+		InvocationException caught = null;
+		try {
+			invoker.invoke(token);
+		} catch (InvocationException x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(MoveFileInvoker.class.getName() + 
+				MoveFileInvoker.CAN_NOT_MOVE_A_FILE_ONTO_ITSELF ,
+				caught.getMessage());
+	}
+	
+	public void testFailMoveFile() {
+		//TODO no idea how to get it to return false
+	}
+
+	public void testMoveFileSuccess() throws Exception {
+		String newDir = DirectorySetup.getTestDirPath() + File.separator + "testToParentDirectoryDoesNOTExist";
+		File file = new File(newDir);
+		assertTrue(file.mkdirs());
+		
+		String childA = newDir + File.separator + "a.txt";
+		File fileA  = new File(childA);
+		assertTrue(fileA.mkdirs());
+		
+		MoveFileToken token = new MoveFileToken();
+		token.setTo_file_path(newDir + File.separator + "b.txt");
+		token.setFrom_file_path(childA);
+		invoker.invoke(token);
+		
+		File b = new File(newDir + File.separator + "b.txt");
+		assertTrue(b.exists());
+	}
+}
