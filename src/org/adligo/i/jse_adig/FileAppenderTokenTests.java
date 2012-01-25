d0e82cc3fafe3e3d4764eb3f6dfbd5a8e62dd0ed
scott <scott@adligo.com>
2012-01-25 03:47:20 +0000

    some new tests

---
+++ src/org/adligo/i/jse_adig/FileAppenderTokenTests.java
@@ -0,0 +1,81 @@
+package org.adligo.i.jse_adig;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.adligo.tests.ATest;
+
+public class FileAppenderTokenTests extends ATest {
+	
+	public void setUp() {
+		DirectorySetup.setUp();
+		JseRegistry.setup();
+	}
+	
+	public void tearDown() {
+		DirectorySetup.tearDown();
+	}
+	
+	public void testNullAppenderSingleLine() {
+		Exception caught = null;
+		try {
+			new FileAppenderToken(null, "hey",true);
+		} catch (Exception x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(FileAppenderToken.FILE_APPENDER_TOKEN_DOES_NOT_ACCEPT_A_NULL_FILE_APPENDER,
+				caught.getMessage());
+	}
+	
+	public void testNullAppenderMultiLine() {
+		Exception caught = null;
+		try {
+			new FileAppenderToken(null, new ArrayList<String>());
+		} catch (Exception x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(FileAppenderToken.FILE_APPENDER_TOKEN_DOES_NOT_ACCEPT_A_NULL_FILE_APPENDER,
+				caught.getMessage());
+	}
+	
+	public void testNullMultiLine() throws IOException {
+		Exception caught = null;
+		FileAppender appender = getFileAppender();
+		try {
+			
+			new FileAppenderToken(appender, null);
+		} catch (Exception x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(FileAppenderToken.FILE_APPENDER_TOKEN_DOES_NOT_ACCEPT_A_NULL_LINES_COLLECTION,
+				caught.getMessage());
+	}
+	
+	public void testNullSingleLine() throws IOException {
+		Exception caught = null;
+		FileAppender appender = getFileAppender();
+		try {
+			
+			new FileAppenderToken(appender, null, true);
+		} catch (Exception x) {
+			caught = x;
+		}
+		assertNotNull(caught);
+		assertEquals(FileAppenderToken.FILE_APPENDER_TOKEN_DOES_NOT_ACCEPT_NULL_DATA,
+				caught.getMessage());
+	}
+	private FileAppender getFileAppender() throws IOException {
+		String testDir = DirectorySetup.getTestDirPath();
+		File file = new File(testDir + File.separator + "foo.txt");
+		file.createNewFile();
+		assertTrue(file.exists());
+		FileWriter writer = new FileWriter(file);
+		
+		return new FileAppender(writer, "\n");
+	}
+}
